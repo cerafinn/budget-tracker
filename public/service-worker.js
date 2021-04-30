6e5b5b00be7a6655cfc763d126b1f00f5870285f
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-30 14:38:14 -0400

    added files to service worker and worked on install function

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,15 +4,30 @@ const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
   "./js/index.js",
-  "./idb.js",
+  "./js/idb.js",
   "./css/style.css",
-  "manifest.json"
+  "./manifest.json",
+  "./icons",
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png"
 ];
 
 self.addEventListener('fetch', function (e) {
 });
 
 self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
 });
 
 self.addEventListener('activate', function(e) {
