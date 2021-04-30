8cdad3141905c7533ff3a8eb0ac33e3baa43ee04
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-30 16:06:46 -0400

    working on offline functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,33 +5,17 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./js/index.js",
   "./js/idb.js",
-  "./css/style.css",
   "./manifest.json",
-  "./icons",
+  "./css/styles.css",
+  // "./icons",
   "./icons/icon-512x512.png",
   "./icons/icon-384x384.png",
   "./icons/icon-192x192.png",
   "./icons/icon-152x152.png",
-  "./icons/icon-144x144.png",
   "./icons/icon-128x128.png",
   "./icons/icon-96x96.png",
   "./icons/icon-72x72.png"
 ];
-
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
-});
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
@@ -59,4 +43,19 @@ self.addEventListener('activate', function(e) {
       );
     })
   );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
 });
