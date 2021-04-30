c8603194e97f1ff446f8041a9e10073f90163afe
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-30 13:21:27 -0400

    working on idb and starting on service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -15,7 +15,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  console.log(event.target.errorCode);
+  console.log('An error occurred : ' + event.target.errorCode);
 };
 
 function saveRecord(record) {
